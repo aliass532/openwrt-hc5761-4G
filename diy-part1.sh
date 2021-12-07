377ea807f3466991efd2b328e9e0ea82c68feb50
aliass532 <56620619+aliass532@users.noreply.github.com>
2021-12-07 20:10:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/aliass532/luci-app-eqos package/eqos
-git clone https://github.com/aswifi/luci-theme-atmaterial_new package/luci-theme-atmaterial_new
+#git clone https://github.com/aswifi/luci-theme-atmaterial_new package/luci-theme-atmaterial_new
